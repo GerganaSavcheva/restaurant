39f1252d59b4ff6b3cb3b07bac5c3eb36178f87d
Patlako <evgenist360@gmail.com>
2022-07-26 13:47:42 +0300

    addedDatabase

--- restaurant/Startup.cs
+++ restaurant/Startup.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using restaurant.Data;
 using restaurant.Services;
 using System;
 using System.Collections.Generic;
@@ -24,6 +26,7 @@ namespace restaurant
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DBRContext>(options => { options.UseSqlServer(Configuration.GetConnectionString("RestaurantCon")); });
             services.AddControllersWithViews();
             services.AddSingleton< PostService>();
 
